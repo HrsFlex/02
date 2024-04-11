46f4f713fd1256c3a66cc58e2fe7706d807733a7
HrsFlex <harsh42026@gmail.com>
2024-04-11 23:14:57 +0530

    unWanted Files removed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+  const [count, setCount] = React.useState(0);
+  console.log(count);
+  console.log(setCount);
+const handleClickInc = () => {
+  setCount(count + 1);
+}
+const handleClickDec = () => {
+  setCount(count - 1);
 }
 
-export default App;
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={handleClickInc}>+</button>
+      <button onClick={handleClickDec}>-</button>
+    </div>
+    
+  )
+}
+
+export default App
